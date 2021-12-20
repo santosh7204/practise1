657e0e37de2934cf8a5c9d26ae33f8c920a82181
santosh <santuyanagunti23.xworkz@gmail.com>
2021-12-20 17:32:49 +0530

    example

---
+++ Animal.java
@@ -0,0 +1,24 @@
+class Animal{ 
+ public static void main(String []args){ 
+   byte[] dog={12,13,32,23,};
+    System.out.println(dog[0]);
+    System.out.println(dog[1]);
+	System.out.println(dog[2]);
+	System.out.println(dog[3]);
+    int[] cat={1,2,3,4};
+	System.out.println(cat[0]);
+	System.out.println(cat[1]);
+	System.out.println(cat[2]);
+	System.out.println(cat[3]);
+   short[] pig={11134,23245,23445,32000};
+	System.out.println(pig[0]);
+	System.out.println(pig[1]);
+	System.out.println(pig[2]);
+	System.out.println(pig[3]);
+   long[] monkey={1234,463838,23322,2339474845l};
+	System.out.println(monkey[0]);
+	System.out.println(monkey[1]);
+	System.out.println(monkey[2]);
+	System.out.println(monkey[3]);
+}
+}
